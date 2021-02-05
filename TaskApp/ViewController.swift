60e6b6fed78c0f821e2a7145a3bdefa829209002
kotaro <kotaro.a0806@gmail.com>
2021-02-05 13:37:24 +0900

    Initial Commit

---
+++ TaskApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskApp
+//
+//  Created by 浅川晃太郎 on 2021/02/05.
+//  Copyright © 2021 kotaro.asakawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
